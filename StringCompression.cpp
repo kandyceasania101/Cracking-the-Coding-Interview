0abb72d19b39864cbfccec1c1cf3aa1b7928d314
Kandyce Burks <kburks2@uic.edu>
2017-11-17 09:03:48 -0600

    1.6

--- StringCompression.cpp
+++ StringCompression.cpp
@@ -1,3 +1,15 @@
+//
+//
+//Objective: Implement a method to perform basic string compression using the counts of repeated characters.
+//For example, the string aabcccccaaa would become a2b1c5a3. If the "compressed" string would not become
+//smaller than the original string, your method should return the original string. You can assume the
+//string has only uppercase and lowercase letter (a-z).
+//
+//Language: C++
+//
+//Kandyce Burks
+//
+
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
